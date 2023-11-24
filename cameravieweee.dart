b556368484df9ef6622fca56c500e59bd72b4c30
hamirdangar <109734153+hamirdangar@users.noreply.github.com>
2023-11-24 15:44:43 +0530

    Update cameravieweee.dart

--- cameravieweee.dart
+++ cameravieweee.dart
@@ -1,1 +1,21 @@
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller.value.isInitialized) {
+      return Container();
+    }
 
+    var tmp = MediaQuery.of(context).size;
+    var screenH = math.max(tmp.height, tmp.width);
+    var screenW = math.min(tmp.height, tmp.width);
+    tmp = controller.value.previewSize;
+    var previewH = math.max(tmp.height, tmp.width);
+    var previewW = math.min(tmp.height, tmp.width);
+    var screenRatio = screenH / screenW;
+    var previewRatio = previewH / previewW;
+
+    return OverflowBox(
+      maxHeight: screenRatio > previewRatio ? screenH : screenW / previewW * previewH,
+      maxWidth: screenRatio > previewRatio ? screenH / previewH * previewW : screenW,
+      child: CameraPreview(controller),
+    );
+  }
